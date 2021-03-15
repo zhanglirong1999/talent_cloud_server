92471da3d70654daa8fd5039c2af7d0bb45362ab
zhanglirong1999 <213182194@seu.edu.cn>
2021-03-15 17:21:16 +0800

    fix

--- src/main/java/seu/talents/cloud/talent/model/dao/entity/Company.java
+++ src/main/java/seu/talents/cloud/talent/model/dao/entity/Company.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,5 +22,6 @@ public class Company {
     @Column(name = "takeCount")
     String takeCount;
     String city;
+    @Column(name = "passRate")
     String passRate;
 }
